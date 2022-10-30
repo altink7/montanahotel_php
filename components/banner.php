7f6b25c98d4e2344c8575d54b2e98119522c4c25
Altin <kelmendialtin48@gmail.com>
2022-10-30 12:48:07 +0100

    banner angepasst

--- components/banner.php
+++ components/banner.php
@@ -1,27 +1,33 @@
 <header>
     <!--Banner start-->
     <div class="Banner">
+
             <?php 
-             echo($page =='index'?'<p>Willkommen bei Montana!</p>':'')
-             ?><?php 
-             echo($page =='zimmer'?'<p>Zimmer</p>':'')
-             ?><?php 
-             echo($page =='about'?'<p>Über Uns</p>':'')
-             ?><?php 
-             echo($page =='impressum'?'<p>Impressum</p>':'')
-             ?><?php 
-             echo($page =='login'?'<p>Login<p>':'')
-             ?><?php 
-             echo($page =='register'?'<p>Registrieren</p>':'')
-             ?><?php 
-             echo($page =='restaurant'?'<p>Restaurant</p>':'')
-             ?><?php 
-             echo($page =='faq'?'<p>Frequently Asked Questions</p>':'')
-            ?><?php 
-            echo($page =='kontakt'?'<p>Kontakt</p>':'')
-            ?><?php 
-            echo($page =='datenschutz'?'<p>Datenschutzerklärung</p>':'')
+            if($page =='index'){
+                echo('<p>Willkommen bei Montana!</p>');
+            }elseif($page =='zimmer'){
+                echo('<p>Zimmer</p>');
+            }elseif($page =='about'){
+                echo('<p>Über Uns</p>');
+            }elseif($page =='impressum'){
+                echo('<p>Impressum</p>');
+            }elseif($page =='login'){
+                echo('<p>Login<p>');
+            }elseif($page =='register'){
+                echo('<p>Registrieren</p>');
+            }elseif($page =='restaurant'){
+                echo('<p>Restaurant</p>');
+            }elseif($page =='faq'){
+                echo('<p>Frequently Asked Questions</p>');
+            }elseif($page =='kontakt'){
+                echo('<p>Kontakt</p>');
+            }elseif($page =='datenschutz'){
+                echo('<p>Datenschutzerklärung</p>');
+            }else{
+                echo('<p>Willkommen bei Montana!</p>');
+            }
             ?>
+            
     </div>
     <!--Banner end-->
 </header>
