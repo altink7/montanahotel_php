8f884906d6f467da39cda1fa11c6498ae54afae6
ALTINLAPTOP\User <wi21b054@technikum-wien.at>
2022-11-02 19:56:20 +0100

    submit impl.

---
+++ submit.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$country=$_POST['country'];
+$address=$_POST['address'];
+$password  =$_POST['password'];
+
+
+$servername= 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'montanadb';
+
+//$mysqli = new mysqli("localhost", "username", "password", "dbname");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+echo "Connection successful!" . "<bc>";
+$sql = "INSERT INTO user (fname, lname) VALUES ('$x', '$y')";
+
+if($conn->query($sql) === TRUE){
+ echo "New record created successfully";
+} else {
+ echo "<bc> Error: " .  $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
