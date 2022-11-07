7c65f2b20f8e50bc892dd1a219921a49bbe6c228
Julian Hoffmann <wi21b050@technikum.at>
2022-11-07 14:25:13 +0100

    zimmer hinzugefügt

--- zimmer.php
+++ zimmer.php
@@ -4,22 +4,113 @@ include 'components/head.php';
 include 'components/nav.php';
 include 'components/banner.php';
 ?>
-
-
     <!--Section 1 start-->
-    <section>
-        <div class="pool">
-            <a> <img class="poolimg" src="img/pool.png"></a>
-            <a> Luxus mitten in der Natur</a>
-        </div>
+	<section>
+        <div class="row">
+        <div class="col-lg-1"></div>
+			<div class="col-lg-5">
+				<div class="room-image">
+					<img class = roomimg src="img/room1.png">
+				</div>
+            </div>
+            <div class="col-lg-1"></div>
+			<div class="col-lg-4 contentroom btn-group-vertical">
+				<h2 class="content-title">Mountain Sweet</h2>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>	
+				<ul class="list-group list-group-horizontal-sm">
+					<li class="list-group-item">42 m²</li>
+					<li class="list-group-item">King-size Bett</li>
+					<li class="list-group-item">Bad, WC, Safe, TV</li>
+				</ul>
+				<div class="content-footer">
+					<div class="row">
+						<span class="col-6 social"><a href="#doppelzimmer"><i class="fa-solid fa-check"></i></a> <input id="buchenButton" type="submit" value="Buchen"></span>
+						<span class="col-6 social text-end">€ 80 / Tag</span>
+					</div>
+				</div>
+			</div>
+            <div class="col-lg-1"></div>
+		</div>
 
-        <div class="food">
-            <a id="foodtxt"> Lassen Sie sich von unseren Sterneköchen verzaubern</a>
-            <a id="foodimg"> <img class="foodimg" src="img/food.png"></a>
-        </div>
+        <div class="row">
+            <div class="col-lg-1"></div>
+			<div class="col-lg-5">
+				<div class="room-image">
+					<img class = roomimg src="img/room2.png">
+				</div>
+            </div>
+            <div class="col-lg-1"></div>
+			<div class="col-lg-4 contentroom btn-group-vertical">
+				<h2 class="content-title">Ozean Sweet</h2>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>	
+				<ul class="list-group list-group-horizontal-sm">
+					<li class="list-group-item">42 m²</li>
+					<li class="list-group-item">King-size Bett</li>
+					<li class="list-group-item">Bad, WC, Safe, TV</li>
+				</ul>
+				<div class="content-footer">
+					<div class="row">
+						<span class="col-6 social"><a href="#doppelzimmer"><i class="fa-solid fa-check"></i></a> <input id="buchenButton" type="submit" value="Buchen"></span>
+						<span class="col-6 social text-end">€ 80 / Tag</span>
+					</div>
+				</div>
+			</div>
+            <div class="col-lg-1"></div>
+		</div>
+    
+    <div class="row">
+        <div class="col-lg-1"></div>
+			<div class="col-lg-5">
+				<div class="room-image">
+					<img class = roomimg src="img/room3.png">
+				</div>
+            </div>
+            <div class="col-lg-1"></div>
+			<div class="col-lg-4 contentroom btn-group-vertical">
+				<h2 class="content-title">Deluxe Villa</h2>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>	
+				<ul class="list-group list-group-horizontal-sm">
+					<li class="list-group-item">42 m²</li>
+					<li class="list-group-item">King-size Bett</li>
+					<li class="list-group-item">Bad, WC, Safe, TV</li>
+				</ul>
+				<div class="content-footer">
+					<div class="row">
+						<span class="col-6 social"><a href="#doppelzimmer"><i class="fa-solid fa-check"></i></a> <input id="buchenButton" type="submit" value="Buchen"></span>
+						<span class="col-6 social text-end">€ 80 / Tag</span>
+					</div>
+				</div>
+			</div>
+            <div class="col-lg-1"></div>
+		</div>
 
-
-    </section>
+        <div class="row">
+            <div class="col-lg-1"></div>
+			<div class="col-lg-5">
+				<div class="room-image">
+					<img class = roomimg src="img/room4.png">
+				</div>
+            </div>
+            <div class="col-lg-1"></div>
+			<div class="col-lg-4 contentroom btn-group-vertical">
+				<a name="mountainsweet"></a>
+				<h2 class="content-title">Ozean Villa</h2>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>	
+				<ul class="list-group list-group-horizontal-sm">
+					<li class="list-group-item">42 m²</li>
+					<li class="list-group-item">King-size Bett</li>
+					<li class="list-group-item">Bad, WC, Safe, TV</li>
+				</ul>
+				<div class="content-footer">
+					<div class="row">
+						<span class="col-6 social"><a href="#doppelzimmer"><i class="fa-solid fa-check"></i></a> <input id="buchenButton" type="submit" value="Buchen"></span>
+						<span class="col-6 social text-end">€ 80 / Tag</span>
+					</div>
+				</div>
+			</div>
+            <div class="col-lg-1"></div>
+		</div>
+    
     <!--Section 1 end-->
   
     <?php
