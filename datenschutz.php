e58ac88ac343b7cfb92e3508fe56e3bd75b75e77
Julian Hoffmann <wi21b050@technikum.at>
2022-11-19 23:44:36 +0100

    datenschutz

--- datenschutz.php
+++ datenschutz.php
@@ -2,42 +2,66 @@
 $page = basename($_SERVER['PHP_SELF'], '.php');
 include 'components/head.php';
 include 'components/nav.php';
-include 'components/banner.php';
+
 ?>
 
+<br><br>
+    <div class="contact text-center">
+        <h1 class="kontaktieren">Datenschutzerklärung</h1><hr>
+                <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                <h4>Rechtsgrundlage Einwilligung</h4>
+                <p>Sie haben uns Daten über sich freiwillig zur Verfügung gestellt und wir verarbeiten diese Daten auf Grundlage Ihrer Einwilligung zu folgenden Zwecken:<br>
+                    Hotelreservierung und Kontaktaufnahme
+                    Sie können diese Einwilligung jederzeit widerrufen. Ein Widerruf hat zur Folge, dass wir Ihre Daten ab diesem Zeitpunkt zu oben genannten Zwecken nicht mehr verarbeiten. Für einen Widerruf wenden Sie sich bitte an: dsgvo(at)businesscity.hotel
 
-    <!--Section pool start-->
-    <div class = "row"></div>
-    <div class="row">
-        <div class="col-lg-2"></div>
-        <div class="col-lg-3">
-                <img class = poolimg src="img/pool.png">
-        </div>
-        <div class="col-lg-1"></div>
-        <div class="col-lg-4 contentroom btn-group-vertical">
-            <a name="mountainsweet"></a>
-            <h2 class="content-title">Atemberaubende Aussicht</h2>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>	
-            </div>
-        </div>
-        <div class="col-lg-1"></div>
-    </div>
-    <!--Section pool end-->
-    <!--Section food start-->
-    <div class="row">
-        <div class="col-lg-2"></div>
-        <div class="col-lg-4 contentroom btn-group-vertical">
-            <a name="mountainsweet"></a>
-            <h2 class="content-title">Haubenküche</h2>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>	
-            </div>
-        <div class="col-lg-1"></div>
-        <div class="col-lg-3">
-            <img class = foodimg src="img/food.png">
-    </div>
-        <div class="col-lg-1"></div>
-    </div><br><br><br>
-    <!--Section food end-->
+<h4>Speicherdauer/Löschungsfrist</h4>
+<p>Wir speichern Ihre Daten bis zu 3 Jahre. Ihre Daten werden nicht an Dritte weitergegeben.
+</p>
+<h4>Kontakt</h4>
+<p>Sie erreichen uns unter folgenden Kontaktdaten: kundendienst@montana.at<br>
+
+Unseren Datenschutzbeauftragten erreichen Sie unter: datenschutz@montana.at
+</p>
+<h4>Rechtsbehelfsbelehrung</h4>
+<p>Ihnen stehen grundsätzlich die Rechte auf Auskunft, Berichtigung, Löschung, Einschränkung, Datenübertragbarkeit und Widerspruch zu. Dafür wenden Sie sich an uns.
+
+Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen Ansprüche sonst in einer Weise verletzt worden sind, können Sie sich bei der Aufsichtsbehörde beschweren. In Österreich ist die Datenschutzbehörde zuständig.
+</p>
+<h4>Aufsichtsbehörde für die Einhaltung datenschutzrechtlicher Bestimmungen in Österreich</h4>
+<p>Österreichische Datenschutzbehörde
+Barichgasse 40-42
+1030 Wien
+Telefon: +43 1 52 152-0
+
+E: dsb@dsb.gv.at
+
+W: https://www.dsb.gv.at
+</p>
+
+<h4>Kontaktadresse rund um das Thema Datenschutzrecht</h4>
+<p>Wirtschaftskammer Wien
+
+Service Center
+Straße der Wiener Wirtschaft 1
+1020 Wien
+T: +43 1 514 50 1010
+
+Kontaktformular: Kontakt zu Rechtsservice - Wirtschafts- und Gewerberecht
+</p>
+<h4>Hinweise</h4>
+<p>Dieser Ratgeber ist auf Gewerbetreibende mit Sitz in Österreich zugeschnitten. Datenverarbeitungen im lebenswichtigen Interesse, in Wahrnehmung einer Aufgabe im öffentlichen Interesse oder in Ausübung öffentlicher Gewalt (Art. 6. Abs. 1 lit. d und lit. f DSGVO) sind daher nicht abgedeckt. Die Weiterverarbeitung von Daten im Sinne des Art. 6 Abs 4 DSGVO ist nicht berücksichtigt.
+Die Inhalte dieses Online-Ratgebers wurden mit größter Sorgfalt erstellt und werden laufend aktualisiert. Für die Richtigkeit, Vollständigkeit, Aktualität oder Qualität der bereitgestellten Informationen und Musterunterlagen sowie auch für weiterführende Links können wir jedoch keine Gewähr übernehmen. Insbesondere erfolgte die Reihung der im Infoblatt auf Basis der Eingaben der Nutzer empfohlen Verfahrensarten ausschließlich nach subjektiven Kriterien und hat keine darüber hinausgehende allgemeingültige Bedeutung. Haftungsansprüche gegen Personen, die Inhalte zu diesem Online-Ratgeber bereit gestellt haben, sind daher ausgeschlossen.
+Wir behalten uns ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen und übernehmen daher keine Gewähr und Haftung für die dauerhafte Verfügbarkeit der dargebotenen Informationen und Musterunterlagen.
+Bei allen personenbezogenen Bezeichnungen gilt die gewählte Form für beide Geschlechter.
+<br><br>
+Datum 19.11.2022 </p>
+</div>
+</div>
+</div>
+        <br><br>
+    
   
     <?php
     include 'components/footer.php';
