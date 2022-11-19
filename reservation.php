2e77cf2e402f185efc1256320ae709480d4d9e67
Julian Hoffmann <wi21b050@technikum.at>
2022-11-19 15:52:44 +0100

    buchen

--- reservation.php
+++ reservation.php
@@ -4,52 +4,61 @@ include 'components/head.php';
 include 'components/nav.php';
 ?>
 
-<div class = "abovecontact"></div>
-    <div class = "contact">
-    <h1 class="kontaktieren">Buchen Sie jetzt Ihren Aufenthalt bei Montana!</h1>
-    <form class="cs-form cs-message">
-    
-        <label class="cform" for="name">Name: </label>
-        <input type="text" class="field field1" placeholder="Name" name="name" id="name required">
-    
-        <label  class="cform" for="email">E-Mail-Adresse: </label>
-        <input type="email" class="field field2" placeholder="E-Mail-Adresse" name="email" id="email">
-    
-        <label class="cform" for="telefonnummer">Telefonnummer: </label>
-        <input type="text" class="field field3" placeholder="Telefonnummer" name="telefonnummer" id="felefonnummer">
-
-        <select class="cform" name="s-select">
-            <option>Zimmer auswählen</option>
-            <option value="1">Mountain Sweet</option>
-            <option value="2">Ozean Sweet</option>
-            <option value="3">Deluxe Villa</option>
-            <option value="4">Ozean Villa</option>
-        </select>
-    
-        <label class="cform" for="telefonnummer">Anreisedatum </label>
-        <input type="date" class="field field3" placeholder="Anreisedatum" name="date" id="date">
-        <label class="cform" for="telefonnummer">Anreisedatum </label>
-        <input type="date" class="field field3" placeholder="Anreisedatum" name="date" id="date">
-                
-        <input type="checkbox" id="fruehtstueck" name="fruehtstueck" value="fruehtstueck">
-        <label for="fruehtstueck">inklusive Frühstück (€ 15 / Tag)</label>
-        <input type="checkbox" id="parkplatz" name="parkplatz" value="parkplatz">
-        <label for="parkplatz">inklusive Parkplatz (€ 10 / Tag)</label>
-        <input type="checkbox" id="haustiere" name="haustiere" value="haustiere">
-        <label for="haustiere">Haustiere?</label>
-        
-                    
-        <label class="cform" for="helfen">Anmerkungen </label>
-        <textarea class="textarea" rows="5" cols="20" placeholder="Möchten Siw uns noch etwas mitteilen?" name="helfen" id="helfen"></textarea>
-    
-       
-        <button class="contactbutton" type="submit">
-            Absenden
-        </button>
-</div>
-
-    </form>
-
+<body>
+    <!-- This is main Form Area Start ++ -->
+    <div class="bf-container text-center">
+        <!-- Main form Body Start -->
+        <div class="bf-body">
+            <!-- Form haed -->
+            <div class="bf-head">
+                <p>Buche jetzt deinen Aufenthalt bei Montana!</p>
+            </div>
+            <!-- Form haed -->
+            <!-- Form Body Box -->
+            <form class="bf-body-box" action="form.php">
+                <div class="bf-row">
+                    <div class="bf-col-6">
+                        <p>Anreisedatum
+                        <input type="date" name="date" id="date"></p>
+                    </div>
+                    <div class="bf-col-6">
+                        <p>Abreisedatum
+                        <input type="date" name="date" id="date"></p>
+                    </div>
+                    <div class="bf-col-6">
+                        <select name="s-select">
+                            <option>Zimmer auswählen</option>
+                            <option value="1">Mountain Sweet</option>
+                            <option value="2">Ozean Sweet</option>
+                            <option value="3">Deluxe Villa</option>
+                            <option value="4">Ozean Villa</option>
+                        </select>
+                    </div>
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+<label for="vehicle1"> inklusive Frühstück (€ 15 / Tag)</label><br>
+<input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+<label for="vehicle2"> inklusive Parkplatz (€ 10 / Tag)</label><br>
+<input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+<label for="vehicle3"> Haustiere?</label><br>
+                </div>
+                <div class="bf-row">
+                    <div class="bf-col-12">
+                        <p>Anmerkungen</p>
+                        <textarea name="Messages" id="Messages" cols="10" rows="2"></textarea>
+                    </div>
+                </div>
+                <div class="bf-row">
+                    <div class="bf-col-3">
+                        <input type="submit" value="Submit">
+                    </div>
+                </div>
+            </form>
+            <!-- Form Body Box -->
+        </div>
+        <!-- Main form Body End -->
+    </div>
+    <!-- This is main Form Area  End -->
+</body>
 <?php
 include 'components/footer.php';
 ?>
