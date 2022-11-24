144388140798f403335507d5dbc37499ddc886a7
Julian Hoffmann <wi21b050@technikum.at>
2022-11-24 20:01:52 +0100

    reservation kleiner

--- reservation.php
+++ reservation.php
@@ -10,19 +10,19 @@ include 'components/nav.php';
     <div class="bf-container text-center">
         <div class="bf-body">
             <div class="bf-head">
-                <h1>Buche jetzt deinen Aufenthalt bei Montana!</h1><br>
+                <h2>Buche jetzt deinen Aufenthalt bei Montana!</h2><br>
             </div>
             <form class="bf-body-box" action="profil.php" method="post">
                 <div class="bf-row">
                     <div class="bf-col-lg-6">
-                        <h4>Anreisedatum
-                        <input type="date" name="from-date" id="from-date"></h4>
+                        <h5>Anreisedatum
+                        <input type="date" name="from-date" id="from-date"></h5>
                     </div>
                     <div class="bf-col-lg-6">
-                        <h4>Abreisedatum
-                        <input type="date" name="to-date" id="to-date"></h4>
+                        <h5>Abreisedatum
+                        <input type="date" name="to-date" id="to-date"></h5>
                     </div> <br>
-                    <div class="bf-col-lg-6 h4">
+                    <div class="bf-col-lg-6 h5">
                         <select name="room-select">
                             <option class="rselect" value="keine Auswahl">Zimmer auswählen</option>
                             <option value="Mountain Sweet">Mountain Sweet</option>
@@ -32,17 +32,17 @@ include 'components/nav.php';
                         </select>
                     </div> <br>
                     <input type="checkbox" id="breakfeast" name="breakfast">
-                    <label for="breakfeast"><h4> inklusive Frühstück (€ 15 / Tag) </h4></label><br>
+                    <label for="breakfeast"><h5> inklusive Frühstück (€ 15 / Tag) </h5></label><br>
 
                     <input type="checkbox" id="parking" name="parking">
-                    <label for="breakfeast"><h4> inklusive Parkplatz (€ 10 / Tag)</h4></label><br>
+                    <label for="breakfeast"><h5> inklusive Parkplatz (€ 10 / Tag)</h5></label><br>
 
                     <input type="checkbox" id="pets" name="pets">
-                    <label for="pets"> <h4>Haustiere?</h4></label><br><br>
+                    <label for="pets"> <h5>Haustiere?</h5></label><br><br>
                 </div>
                 <div class="bf-row">
                     <div class="bf-col-lg-12">
-                        <h4>Anmerkungen</h4>
+                        <h5>Anmerkungen</h5>
                         <textarea name="messages" id="messages" cols="45" rows="5"></textarea>
                     </div>
                 </div>
