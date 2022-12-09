c222119b48ac2cc632a283ab9ba496df69bb56cf
Julian Hoffmann <wi21b050@technikum.at>
2022-12-09 17:29:22 +0100

    password-db

--- submit.php
+++ submit.php
@@ -6,7 +6,7 @@ $username=$_POST['vorname'];
 $useremail=$_POST['email'];
 $password  =password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-$conn = new mysqli($host, $user, $password_global, $database);
+$conn = new mysqli($host, $user, $password_db, $database);
 
 echo "Connection successful!" . "<bc>";
 $sql = "INSERT INTO users (username, useremail, password) VALUES ('$username', '$useremail','$password_db')";
