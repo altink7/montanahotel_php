897186b0ae944612db74044d924e6ab729062027
altink7 <kelmendialtin48@gmail.com>
2022-12-09 17:31:03 +0100

    password

--- submit.php
+++ submit.php
@@ -9,7 +9,7 @@ $password  =password_hash($_POST['password'], PASSWORD_DEFAULT);
 $conn = new mysqli($host, $user, $password_db, $database);
 
 echo "Connection successful!" . "<bc>";
-$sql = "INSERT INTO users (username, useremail, password) VALUES ('$username', '$useremail','$password_db')";
+$sql = "INSERT INTO users (username, useremail, password) VALUES ('$username', '$useremail','$password')";
 
 if($conn->query($sql) === TRUE){
  echo "New record created successfully";
